88b387e3112449a824cf34339d9b661aca5fe539
Nikita Ivanov <nikita-ivanov.mail@yandex.ru>
2018-05-13 02:54:04 +0300

    Added my done tasks
    And also corrected 1.10 to my solution

--- task-01/task-01-10.erl
+++ task-01/task-01-10.erl
@@ -1,14 +1,20 @@
-(C,[3])abcde = aebcd
+С[3] abcde = aebcd
 
 aebcd
 Cabecd
 C(Cab)ced
-BC(Ca)bced
-B(BC)Cabced
-C(B(BC)Cabc)de
-B(B(BC)Cab)cde
-B(B(BC)Ca)bcde
-B(B(B(BC)C)abcde
-S(KS)K(S(KS)K(S(KS)K(S(KS)KS(S(KS)KS(KS)KS)(KK))S(S(KS)KS(KS)KS)(KK))abcde
+C(C(Cab)c) de
+C ( (C(Cab)) c ) de
+BC(C(Cab)) cde
+BC( C((Ca) b) ) cde
+BC( BC(Ca)b ) cde
+(BC) ( (BC(Ca)) b ) cde
+B(BC)(BC(Ca)) bcde
+B(BC)( (BC)(Ca) ) bcde
+B(BC)( B(BC)Ca ) bcde
+(B(BC))( (B(BC)C)a ) bcde
+B(B(BC))(B(BC)C) abcde
 
-(C,[3]) = S(KS)K(S(KS)K(S(KS)K(S(KS)KS(S(KS)KS(KS)KS)(KK))S(S(KS)KS(KS)KS)(KK))
+C[3] = B(B(BC))(B(BC)C),
+B = S(KS)K,
+C = S(BBS)(KK)
